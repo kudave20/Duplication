9d8869db20b71d67d3663da3eb7d5f637cc3b803
DY <dave1315@korea.ac.kr>
2023-06-24 19:57:09 +0900

    프로젝트 생성

---
+++ Source/PuzzleNameD/PuzzleNameDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PuzzleNameDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLENAMED_API APuzzleNameDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
